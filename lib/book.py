6c2112b4dd18688627ce8c30c3b73c22b7ffbaac
cynthiawuor <cynthiawuor254@gmail.com>
2023-09-19 14:11:04 +0300

    oop

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def get_title(self):
+        return self.title
+
+    def get_author(self):
+        return self.author
+
+    def get_pages(self):
+        return self.pages
+
+    # You can add additional methods as needed
+
+# If you need to add more methods or properties to the Book class, feel free to do so based on the lab requirements.
+
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
+
+    def get_brand(self):
+        return self.brand
+
+    def get_size(self):
+        return self.size
+
+    def get_color(self):
+        return self.color
+
     pass
